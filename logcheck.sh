3b8b7583d9319384dd1692f81a58e9cdeef6f610
Shane Blaufuss <shane.blaufuss@raytheon.com>
2014-10-08 13:18:40 -0500

    Fixed bugs, first working version ready to be deployed.

--- logcheck.sh
+++ logcheck.sh
@@ -13,13 +13,10 @@
 # [Ddd Mmm DD hh:mm:ss YYYY]
 # We're looking for "[Ddd Mmm DD" only
 
-# Debug mode
-set -x
-
 DATE=`date +'%m.%d.%y'`
 #LOGDATE="[`date +'%a %b %d'`"
 MAX_LOGS=5 # Keep 5 days' worth
-EVIDENCE="\{\ \:\;\}\;"
+EVIDENCE="{ :;};"
 HOSTNAME=`hostname`
 FILENAME="$DATE.log"
 GREP=/bin/fgrep
@@ -39,11 +36,12 @@ function usage
 
 function count_files 
 { # Used to count amount of existing files and how many we need to remove in the logrotate function
-	EFILES=`ls -tr $OUTPUT` # Create an array of filenames, sorted in order of oldest first
-        NUM_FILES=${EFILES[@]} # $NUM_FILES is the number of files in the $OUTPUT directory, counted by
+	EFILES=($(find $OUTPUT -type f -print0 |xargs -0 ls -tr)) # Create an array of filenames, sorted in order of oldest first
 # the array we created above. 
-        DFILES=$NUM_FILES - $MAX_FILES # DFILES is the number of files we need to delete to have 
-# $MAX_FILES amount of files in $OUTPUT. If EFILES has 7 files listed, then DFILES will be 2.
+	COUNT=${#EFILES[@]}
+        DFILES=$((COUNT - MAX_LOGS)) # DFILES is the number of files we need to delete to have 
+# $MAX_LOGS amount of files in $OUTPUT. If EFILES has 7 files listed, then DFILES will be 2. This line subtracts $MAX_LOGS from the number of files contained in $EFILES. So, if $MAX_LOGS is 5 and $EFILES has 7 items in it, the result here is 2, and 2 files should be deleted.
+# If COUNT is 1, then 1 - 5 is -4, and nothing needs to be deleted. If COUNT is 5, 5-5 = 0 and 1 should be deleted.
 	return 0
 }
 
@@ -66,11 +64,11 @@ function get_args
 			;;
 			
 			o) # -o was detected, put the output location in another variable
-				if grep -q "\@" $OPTARG # the argument to -o is an email address
+				if [[ $OPTARG = *@* ]]  # the argument to -o is an email address
 					then
 						EMAIL=$OPTARG
 					else
-						OUTPUT="${OPTARG}/${HOSTNAME}/"
+						OUTPUT="${OPTARG}/${HOSTNAME}"
 				fi
 			;;
 			h|*) # Help requested
@@ -84,14 +82,14 @@ function extract_logs
 { # This part will scan the logs looking for BASH exploits, and record them in a file or email
 	if [ -z $OUTPUT ] #if the output variable is not set, then email logs
 	then
-		$GREP $EVIDENCE $LOGFILE |mailx -s "bash exploits found on `hostname` $EMAIL" 
+		$GREP -n -H "$EVIDENCE" $LOGFILE |mailx -s "bash exploits found on `hostname`" $EMAIL
 	else # $OUTPUT is defined, meaning that an email address was not found so it must be a filename
-		if [ -x $OUTPUT ] #if the directory doesn't exist, create it
+		if [ ! -x $OUTPUT ] #if the directory doesn't exist, create it
 		then
 			mkdir -p $OUTPUT
 		fi
 		rotate_logs
-		$GREP $EVIDENCE $LOGFILE >> "$OUTPUT/$FILENAME"
+		$GREP -n -H "$EVIDENCE" $LOGFILE >> "$OUTPUT/$FILENAME"
 	fi
 }
 
@@ -99,19 +97,18 @@ function rotate_logs
 { 
 	count_files
 	if [ $DFILES -ge 0 ] 
-# If there are $MAX_FILES, or more, of files in the log directory.  We'll need to delete at least one 
-# (DFILES=0) or more (DFILES>0)
+# If the number of files to delete is 0 or more, do this:
 	then
 
 # We're going to step through the $EFILES array up to the number of $DFILES, pull a filename from 
 # the EFILES array by index number, and then delete that file.
-		while [$DFILES -ge 0] # $DFILES is at least 0
+		until [ $DFILES -eq 0 ] # Do this loop until $DFILES is -1 
 		do
 			for((i=0; i <= $DFILES; i++)) # Step through $DFILES amount of elements
 			{	
-				rm "$OUTPUT/${EFILES[$i]}" # use absolute pathname
+				rm "${EFILES[$i]}" # use absolute pathname
+			DFILES=$((DFILES - 1))
 			}
-			count_files; # Recheck file counts to either stay in the loop or exit it.
 		done
 	else
 		# Less than max amount of files present; go ahead and write one by returning to extract_logs.
@@ -124,7 +121,7 @@ function rotate_logs
 
 #######
 # Main section
-######
+#######
 
 get_args # Figure out what we need to do
 extract_logs	# filter for evidence, and either email it or write it to a directory defined in $OUTPUT
